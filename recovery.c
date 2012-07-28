b3ac183a0ddf8f9fb7cfb59ffb26e60c85d4e857
stratosk <stratosk@semaphore.gr>
2012-07-28 21:15:03 +0300

    Added AROMA Filemanager

--- recovery.c
+++ recovery.c
@@ -67,6 +67,7 @@ static int poweroff = 0;
 static const char *SDCARD_PACKAGE_FILE = "/sdcard/update.zip";
 static const char *TEMPORARY_LOG_FILE = "/tmp/recovery.log";
 static const char *SIDELOAD_TEMP_DIR = "/tmp/sideload";
+static const char *AROMAFM = "/tmp/aromafm.zip";
 
 extern UIParameters ui_parameters;    // from ui.c
 
@@ -751,7 +752,11 @@ prompt_and_wait() {
                 show_advanced_menu();
                 break;
                 
-            case ITEM_POWEROFF:
+            case ITEM_AROMAFM:
+                install_zip(AROMAFM);
+                break;
+
+	    case ITEM_POWEROFF:
                 poweroff = 1;
                 return;
         }
